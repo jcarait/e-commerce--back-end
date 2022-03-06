7077b29818a9af0a6e854f57e6fc832f037f5209
Jonathan Florido-Carait <jonathan.carait@outlook.com>
2022-03-06 18:17:07 +1100

    updated category and product models

--- models/Product.js
+++ models/Product.js
@@ -2,6 +2,7 @@
 const { Model, DataTypes } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
+const seedCategories = require('../seeds/category-seeds');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
 class Product extends Model {}
@@ -9,7 +10,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: category-seeds,
+        key: id,
+      }
+    },
+
   },
   {
     sequelize,
